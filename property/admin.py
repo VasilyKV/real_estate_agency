54ffd185d76e1e5ccc4c8089356e7273c49ca0af
VasilyKV <103107192+VasilyKV@users.noreply.github.com>
2022-08-28 20:31:41 +0300

    m2m in Admin

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+class OwnerFlatInline(admin.TabularInline):
+	model = Owner.flats.through
+	raw_id_fields = ('flat','owner')
+
 class FlatAdmin(admin.ModelAdmin):
 	search_fields = ('town', 'address',)
 	readonly_fields = ('created_at',)
@@ -9,14 +13,17 @@ class FlatAdmin(admin.ModelAdmin):
 	list_editable = ('new_building',)
 	list_filter = ('new_building', 'rooms_number', 'has_balcony' )
 	raw_id_fields = ('liked_by',)
+	inlines = [OwnerFlatInline,]
 
 class ComplaintAdmin(admin.ModelAdmin):
 	raw_id_fields = ('user', 'flat',)
 
 class OwnerAdmin(admin.ModelAdmin):
-	raw_id_fields = ('flats',)
+	# raw_id_fields = ('flats',)
 	list_display = ('name', 'pure_phone')
 	search_fields = ('name', 'pure_phone')
+	inlines = [OwnerFlatInline,]
+	exclude = ('flats',)
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
